afb837a7b4d4ec58fc407b343e584aa928c1997f
skoech <sharonkoech5147@gmail.com>
2021-08-28 15:36:42 +0300

    Add Versioning and localisation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,10 +3,58 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  customFields: {
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
+  title: 'Tremor',
+  tagline: 'An early-stage event processing system for unstructured data with rich support for structural pattern-matching, filtering and transformation.',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'docs/favicon.ico',
+  organizationName: 'tremor-rs', 
+  projectName: 'tremor-new-website', 
+  themeConfig: {
+    navbar: {
+      logo: {
+        alt: 'Tremor Logo',
+        src: 'https://www.tremor.rs/img/common/logo.png',
+        href: 'https://your-docusaurus-test-site.com',
+        target: '_self',
+      },
+      items: [
+        { to: 'getting-started/',
+          label: 'Getting Started',
+          position: 'left',
+    	},
+          {
+	          type: 'doc',
+            docId: 'index',
+            label: 'Docs',
+            position: 'left',
+          },
+	       {to: 'blog', label: 'Blog', position: 'left'},
+         {to: 'workshop', label: 'Usage Guide', position: 'left'},
+         {to: 'api', label: 'API', position: 'left'},
+	       {to: 'rfcs', label: 'RFCs', position: 'left'},
+	       {to: 'community', label: 'Community', position: 'left'},
+	       {to: 'governance', label: 'Governance', position: 'left'},
+         {to: 'faqs', label: 'FAQs', position: 'left' },
+        {
+          href: 'https://github.com/tremor-rs',
+          label: 'GitHub',
+          position: 'right',
+          className: 'header-github-link',
+          'aria-label': 'GitHub',
+        },
+        {
+          type: 'docsVersionDropdown',
+          position: 'left',
+          dropdownItemsAfter: [{to: 'docs/', label: 'All versions'}],
+        },
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
+      ],
     },
     footer: {
       style: 'dark',
@@ -22,8 +70,8 @@ module.exports = {
               label: 'Twitter',
               href: 'https://twitter.com/TremorDEBS',
             },
-	  ],
-	}, 
+          ],
+        },
         {
           title: 'More',
           items: [
@@ -34,66 +82,15 @@ module.exports = {
             {
               label: 'CNCF',
               href: 'https://www.cncf.io/',
-            },
-                ],
+            }
+          ],
         },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Tremor`,
     },
-  },
-  title: 'Tremor',
-  tagline: 'An early-stage event processing system for unstructured data with rich support for structural pattern-matching, filtering and transformation.',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'ignore',
-  onBrokenMarkdownLinks: 'ignore',
-  favicon: 'docs/favicon.ico',
-  organizationName: 'tremor-rs', 
-  projectName: 'tremor-new-website', 
-  themeConfig: {
-    navbar: {
-      logo: {
-        alt: 'Tremor Logo',
-        src: 'https://www.tremor.rs/img/common/logo.png',
-        href: 'https://your-docusaurus-test-site.com',
-        target: '_self',
-      },
-      items: [
-        {to: 'getting-started/',
-        label: 'Getting Started',
-        position: 'left',
-        },
-	      {
-	        type: 'doc',
-          docId: 'index',
-          label: 'Docs',
-          position: 'left',
-        },
-        /*{to: 'docs', docId: 'index', type: 'doc', label: 'Docs', position: 'left'},*/
-	      {to: 'blog', label: 'Blog', position: 'left'},
-        {to: 'workshop', label: 'Usage Guide', position: 'left'},
-        {to: 'api', label: 'API', position: 'left'},
-	      {to: 'rfcs', label: 'RFCs', position: 'left'},
-	      {to: 'community', label: 'Community', position: 'left'},
-	      {to: 'governance', label: 'Governance', position: 'left'},
-	      {to: 'faqs', label: 'FAQs', position: 'left' },
-
-        {
-          type: 'docsVersionDropdown',
-          position: 'right',
-        
-        },
-        {
-          type: 'localeDropdown',
-          position: 'right',
-        },
-        {
-          href: 'https://github.com/tremor-rs',
-          position: 'right',
-          className: 'header-github-link',
-          'aria-label': 'GitHub',
-        },
-          ],
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
     },
   },
   presets: [
