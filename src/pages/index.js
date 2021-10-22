36dbf7217b047952afe26271b0bf2cc836fa1736
CJ Gilroy <christopher.g@getjobber.com>
2021-10-22 16:54:49 -0600

    Move getting started from community to docs
    Signed-off-by: CJ Gilroy <christopher.g@getjobber.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="community/getting-started/getting-started">
+            to="docs/next/getting-started/getting-started">
             Getting Started
           </Link>
         </div>
