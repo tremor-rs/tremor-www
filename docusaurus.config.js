44046267196a34a217880e24c0ef1bea2da3aa6c
Darach Ennis <dennis@wayfair.com>
2021-10-28 12:19:58 +0200

    Fix netlify logo issue and add to footer
    Signed-off-by: Darach Ennis <dennis@wayfair.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -149,9 +149,9 @@ module.exports = {
             {
               html: '<a href="https://www.cncf.io/" rel="noreferrer noopener" aria-label="CNCF"><img src="/img/cncf-color.svg" alt="CNCF" style="width: 70%;" /></a>',
             },
-            // {
-            //   html: '<a href="https://www.netlify.com" rel="noreferrer noopener" aria-label="Netlify"><img src="/img/netlify-color-accent.svg" alt="Deploys by Netlify" style="width: 40%;" /></a>',
-            // },
+            {
+              html: '<a href="https://www.netlify.com" rel="noreferrer noopener" aria-label="Netlify"><img src="/img/netlify-full-logo.svg" alt="Deploys by Netlify" style="width: 40%;" /></a>',
+            },
           ],
         },
       ],
