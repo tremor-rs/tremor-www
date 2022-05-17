55bd2e5ff6144aecabcf9d59cb8b429c67e7a99d
Heinz N. Gies <heinz@licenser.net>
2022-05-17 11:58:14 +0200

    Fix redocusaurs use
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="docs/0.12/getting-started/">
+            to="docs/edge/getting-started/">
             Getting Started
           </Link>
         </div>
