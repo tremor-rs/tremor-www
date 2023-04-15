de0d4b56b00b716a0309e27bf908c70427ea90be
Sharon Koech <sharonkoech5147@gmail.com>
2023-04-15 16:22:05 +0300

    Change browser tab title for homepage
    Signed-off-by: Sharon Koech <sharonkoech5147@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Event Processing System for Unstructured Data`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
