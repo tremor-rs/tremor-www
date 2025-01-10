94c1e7bda3252537ece5321b2ed7339aa00953a0
Heinz N. Gies <heinz@licenser.net>
2025-01-10 12:52:15 +0100

    update prism renderer config
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 const gitLinks = require('./src/remark/git-links');
-const lightCodeTheme = require('prism-react-renderer/themes/dracula');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer').themes.dracula;
+const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
